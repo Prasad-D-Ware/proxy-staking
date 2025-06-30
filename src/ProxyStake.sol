268e6f8fe9c13dcd5f75e772d0f6aadbd43c71f4
Prasad-D-Ware <prasadware42116@gmail.com>
2025-06-30 23:58:27 +0530

    feat : basic stake contract

--- src/ProxyStake.sol
+++ src/ProxyStake.sol
@@ -2,8 +2,30 @@
 pragma solidity ^0.8.0;
 
 contract ProxyStake {
-    
+    uint public totalBalance;
+    address public owner;
+    mapping(address => uint) balances;
+
     constructor() {
         // constructor
+        owner = msg.sender;
+    }
+
+    function stake() public payable {
+        require(msg.value > 0,"Amount should be greater than 0");
+        balances[msg.sender] += msg.value;
+        totalBalance += msg.value;
+    }
+
+    function unstake(uint _amount) public {
+        require(balances[msg.sender] >= _amount, "Not staked this much eth");
+        totalBalance-=_amount;
+        balances[msg.sender] -= _amount;
+        (bool success, ) = msg.sender.call{value : _amount}("");
+        require(success);
+    }
+
+    function getBalace() public view returns (uint){
+        return totalBalance;
     }
 }
