4f9919c19512243dad95c7740c539778fe2f70cd
Prasad-D-Ware <prasadware42116@gmail.com>
2025-07-01 23:42:46 +0530

    feat : tests for stake unstake

--- test/ProxyStake.t.sol
+++ test/ProxyStake.t.sol
@@ -7,7 +7,24 @@ import "forge-std/Test.sol";
 contract ProxyStakeTest is Test {
     ProxyStake public proxyStake;
 
+    receive() external payable {}
+
     function setUp() public {
         proxyStake = new ProxyStake();
     }
+    
+    function test_stake() public {
+        proxyStake.stake{value : 10 ether}();
+        assert(proxyStake.totalBalance() == 10 ether);
+    }
+
+    function test_unstake() public {
+        proxyStake.stake{value : 10 ether}();
+        assert(proxyStake.totalBalance() == 10 ether);
+
+        proxyStake.unstake(5 ether);
+
+        assert(proxyStake.totalBalance() == 5 ether);
+    }   
+
 }
