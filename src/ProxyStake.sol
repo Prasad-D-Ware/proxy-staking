e4e45e663610adfd0a206a8ca16a344bd5f458dd
Prasad-D-Ware <prasadware42116@gmail.com>
2025-07-08 23:16:50 +0530

    feat : stake contract with rewards calc

--- src/ProxyStake.sol
+++ src/ProxyStake.sol
@@ -1,31 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface ITokenAddress {
+    function mint(address _reciever, uint _value) external;
+}
+
 contract ProxyStake {
     uint public totalBalance;
     address public owner;
     mapping(address => uint) balances;
+    address public tokenAddress;
+    mapping(address => uint) rewards;
+    mapping(address => uint) updatedAt;
+    uint REWARD_PER_ETH = 1;
 
     constructor() {
         // constructor
         owner = msg.sender;
     }
 
+    function _updateRewards(address _user) internal {
+        if (updatedAt[_user] == 0) {
+            updatedAt[_user] = block.timestamp;
+            return;
+        }
+        uint currentReward = rewards[_user];
+        uint timeDiff = block.timestamp - updatedAt[_user];
+
+        if (timeDiff == 0) {
+            return;
+        }
+        uint newReward = timeDiff * balances[_user] * REWARD_PER_ETH;
+        rewards[_user] += currentReward + newReward;
+        updatedAt[_user] = block.timestamp;
+    }
+
     function stake() public payable {
-        require(msg.value > 0,"Amount should be greater than 0");
+        require(msg.value > 0, "Amount should be greater than 0");
+        _updateRewards(msg.sender);
         balances[msg.sender] += msg.value;
         totalBalance += msg.value;
     }
 
     function unstake(uint _amount) public {
         require(balances[msg.sender] >= _amount, "Not staked this much eth");
-        totalBalance-=_amount;
+        _updateRewards(msg.sender);
         balances[msg.sender] -= _amount;
-        (bool success, ) = msg.sender.call{value : _amount}("");
+        totalBalance -= _amount;
+        (bool success, ) = msg.sender.call{value: _amount}("");
         require(success);
     }
 
-    function getBalace() public view returns (uint){
+    function getTotalBalace() public view returns (uint) {
         return totalBalance;
+    }
+
+    function getBalanceOf(address _address) public view returns (uint) {
+        return balances[_address];
+    }
+
+    function setTokenAddress(address _tokenAddress) public {
+        require(msg.sender == owner, "Only owner can set token address");
+        tokenAddress = _tokenAddress;
+    }
+
+    function claimReward() public {
+        _updateRewards(msg.sender);
+        ITokenAddress(tokenAddress).mint(msg.sender, rewards[msg.sender]);
+        rewards[msg.sender] = 0;
+    }
+
+    function getRewardAvailable() public view returns (uint) {
+        uint timeDiff = block.timestamp - updatedAt[msg.sender];
+        if (timeDiff == 0) {
+            return rewards[msg.sender];
+        }
+
+        uint reward = (timeDiff * balances[msg.sender] * REWARD_PER_ETH) +
+            rewards[msg.sender];
+        return reward;
     }
 }
